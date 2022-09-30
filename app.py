8b7cdf6a4fcbe0db83ced31620bb2af4357a5e15
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2022-10-01 00:02:46 +0530

    App pushed

---
+++ app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import random
+import string
+import subprocess
+from flask import Flask, request
+
+app = Flask(__name__)
+
+def randName():
+    return ''.join([random.choice(string.hexdigits) for i in range(16)])
+
+@app.route('/')
+def index():
+    dbpath = f'/tmp/{randName()}.db'
+
+    query = request.args.get("query")
+
+    if query:
+        ban = ['.', 'lo', ';', 'read', 'im']
+
+        for x in ban:
+            if x in query:
+                return "Filtered.."
+
+        proc = subprocess.Popen(["sqlite3", dbpath, query], stdout=subprocess.PIPE)
+        (out, err) = proc.communicate()
+
+        return out
+    else:
+        return 'no query'
+
+app.run(host='0.0.0.0', port=9000)
