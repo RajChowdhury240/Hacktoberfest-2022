3a6b1688880c7df97b7543f1b9003aa669ff26ec
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2022-10-01 00:02:07 +0530

    Python reverse added

---
+++ shufflearoo.py
@@ -0,0 +1,17 @@
+from flag import flag
+import random
+b = 1
+c = 2
+secret = random.randint(1, 10000)
+d = len(flag)
+a = d
+e = 0
+
+
+while e < d * secret:
+    flag = flag[0:c] + chr(ord(flag[c])+a) + flag[c+1:]
+    a = (a + b) % 127
+    b = b + a
+    c = (c + a) % d
+    e += 1
+print(":".join("{:02x}".format(ord(char)) for char in flag))
